24c4b69794cd477e573243da8d03b1f6403354ee
Shamel Lakin <christlife4u@gmail.com>
2019-10-15 12:09:22 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,21 @@
+class Patient 
+  
+  @@all = []
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end 
+  
+  def new_appointment(doctor, date)
+     Appointment.new(date, self, doctor)
+  end 
+  
+  def self.all 
+    @@all 
+  end
+  
+  
+end 
