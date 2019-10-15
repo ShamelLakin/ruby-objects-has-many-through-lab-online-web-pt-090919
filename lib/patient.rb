358b8621d2bb464e79efe7f2e158c3fd34ea5633
Shamel Lakin <christlife4u@gmail.com>
2019-10-15 12:20:46 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -13,7 +13,7 @@ class Patient
   end
   
   def appointments
-    Appointment.all.select{|appointments| appointments.doctor == self}
+    Appointment.all.select{|appointments| appointments.patient == self}
   end 
   
   
